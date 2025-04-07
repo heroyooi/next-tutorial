d43558b29d6dbf21b04b08d54e04381268cb20be
heroyooi <heroyooi1018@gmail.com>
2025-04-07 16:53:24 +0900

    📘 챕터 C. 정적 페이지 만들기 - 퍼블리셔 작업물을 Next.js에 넣는 법

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,11 @@
+import styles from './landing.module.css'
+
+export default function LandingPage() {
+  return (
+    <section className={styles.hero}>
+      <h1>Welcome to My Site</h1>
+      <p>This is a simple landing page.</p>
+    </section>
+  )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ export default function Home() {
       <h1>안녕하세요, 학생 여러분!</h1>
       <p>Next.js로 만든 나의 첫 웹페이지 🎉</p>
 
-      <Link href="/about">소개 페이지로 이동하기</Link>
-      <Link href="/contact">연락 페이지로 이동하기</Link>
+      <Link href="/about">소개 페이지로 이동하기</Link><br />
+      <Link href="/contact">연락 페이지로 이동하기</Link><br />
+      <Link href="/landing">랜딩 페이지로 이동하기</Link>
     </main>
   )
 }
